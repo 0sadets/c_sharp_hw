d3da3690a868c6bc8d77dd714a966fb6c79ecaff
0sadets <osadets_ak21@nuwm.edu.ua>
2024-04-27 18:37:14 +0300

    done 4 hw

---
+++ HW4_Intro_to_OOP/Freezer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_Intro_to_OOP
+{
+    partial class Freezer
+    {
+        static Freezer()
+        {
+            color = "white";
+            model = "model";
+        }
+        public Freezer()
+        {
+            maxTemp = 0;
+            minTemp = 0;
+            width = 0;
+            height = 0;
+        }
+        public Freezer(int maxT, int minT)
+        {
+            if (maxT < 30) maxTemp = maxT;
+            else maxTemp = 0;
+
+            if (minT > -50) MinTemp = minT;
+            else MinTemp = 0;
+        }
+        public Freezer(int width, int height, int maxT, int minT) : this(maxT, minT)
+        {
+            setHeight(height); setWidth(width);
+        }
+
+        public void setWidth(int w)
+        {
+            if (w > 0)
+            {
+                width = w;
+            }
+            else { width = 50; }
+        }
+        public void setHeight(int height)
+        {
+            if (height > 0)
+            {
+                this.height = height;
+            }
+            else { this.height = 100; }
+        }
+        public override string ToString()
+        {
+            return $"Max temperature: {maxTemp}, Min temperature: {minTemp}.\nWidth: {width}, Height: {height}.\nModel: {model}, Color: {color}";
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Max temperature: {maxTemp}, Min temperature: {minTemp}.\nWidth: {width}, Height: {height}.\nModel: {model}, Color: {color}"); ;
+        }
+    }
+}
---
+++ HW4_Intro_to_OOP/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Drawing;
+
+namespace HW4_Intro_to_OOP
+{
+    partial class Freezer
+    {
+        private int maxTemp;
+        private int minTemp;
+
+        private int width;
+        private int height;
+        
+        static string model;
+        static string color;
+        public int MaxTemp
+        {
+            get
+            {
+                return maxTemp;
+            }
+            set
+            {
+                if (value < 20)
+                {
+                    maxTemp = value;
+                }
+                else
+                {
+                    maxTemp = 0;
+                }
+            }
+        }
+        public int MinTemp
+        {
+            get
+            {
+                return minTemp;
+            }
+            set
+            {
+                if (value > -50)
+                {
+                    minTemp = value;
+                }
+                else
+                {
+                    minTemp = 0;
+                }
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    width = 50;
+                }
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    height = 100;
+                }
+            }
+        }
+
+        
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Freezer[] freezer = new Freezer[5];
+            freezer[0] = new Freezer();
+            freezer[1] = new Freezer(70, -10);
+            freezer[2] = new Freezer(60, 140, 14, -25);
+            freezer[3] = new Freezer(25, -40);
+            freezer[3].Width = 50;
+            freezer[3].Height = 260;
+            freezer[4] = new Freezer();
+            freezer[4].MaxTemp = 40;
+            freezer[4].MinTemp = -30;
+            freezer[4].Width = 70;
+            freezer[4].Height = 260;
+            for(int i = 0; i< 5; i++)
+            {
+                Console.WriteLine(freezer[i].ToString());
+                //freezer[i].Print();
+                Console.WriteLine();
+            }
+        }
+    }
+}
